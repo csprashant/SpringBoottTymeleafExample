925cfeda51060b446d1efad39b38d6dff694e165
csprashant <prashant.puse@innodeed.com>
2023-12-23 20:14:57 +0530

    minor fixes

--- src/main/java/com/inno/Controller.java
+++ src/main/java/com/inno/Controller.java
@@ -1,7 +1,8 @@
 package com.inno;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,25 @@ public class Controller {
 	public String generateStringHtmlContent() {
 		System.out.println("Request Received");
 		Map<String, Object> context = new HashMap<>();
-		context.put("memberName", "Spring Boot");
-		context.put("message", "This is a sample message");
-		context.put("timestamp", new Date());
-		String content =  templateService.generateTemplate("invoice_payment_failed", context);
+		context.put("empsList", getDetails());
+		String content =  templateService.generateTemplate("emp", context);
 		System.out.println(content);
 		return content;
 
+	}
+	
+	public List<Employee> getDetails(){
+		List<Employee> list = new ArrayList<Employee>();
+		list.add(new Employee(101,"Raj Name to suna hi hoga",345450.00));
+		list.add(new Employee(102,"Rajesh",36000.00));
+		list.add(new Employee(103,"Raja",56000.00));
+		list.add(new Employee(104,"Raju",26000.00));
+		list.add(new Employee(105,"Rajat",96000.00));
+		list.add(new Employee(107,"Golu",76000.00));
+		list.add(new Employee(108,"Rajiv",16000.00));
+		list.add(new Employee(109,"Sonu",46000.00));
+		list.add(new Employee(110,"Tanu",56000.00));
+		return list;
+		
 	}
 }
---
+++ src/main/java/com/inno/Employee.java
@@ -0,0 +1,53 @@
+package com.inno;
+
+public class Employee {
+	private Integer empno;
+	private  String ename;
+	private  String job;
+	private  Double sal;
+	private  Integer deptno=10;
+	public Integer getEmpno() {
+		return empno;
+	}
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public Double getSal() {
+		return sal;
+	}
+	public void setSal(Double sal) {
+		this.sal = sal;
+	}
+	public Integer getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(Integer deptno) {
+		this.deptno = deptno;
+	}
+
+public Employee() {
+	// TODO Auto-generated constructor stub
+}
+public Employee(Integer empno, String ename, Double sal) {
+	super();
+	this.empno = empno;
+	this.ename = ename;
+	this.job = "Developer";
+	this.sal = sal;
+	this.deptno = 10;
+}
+
+
+}
